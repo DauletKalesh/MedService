4d3711cfd1255e7893e42a0ad7a78ac17ca5cc11
Зарина <Zarina@MacBook-Pro-Zarina.local>
2022-12-02 03:40:11 +0600

    added new models

--- med_management/admin.py
+++ med_management/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Hospital, Medical_history, Appointment
 
 # Register your models here.
+admin.site.register(Hospital)
+admin.site.register(Medical_history)
+admin.site.register(Appointment)
