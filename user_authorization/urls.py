560f58c3fd98dc3021a11553a7f8e4247de76bc4
DauletKalesh <dauletkalesov@gmail.com>
2022-12-05 18:38:29 +0600

    some comment

--- user_authorization/urls.py
+++ user_authorization/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import AdvancedUserViewSet, LoginView, ProfileView
 
+
 urlpatterns = [
     path('registration', AdvancedUserViewSet.as_view({'post': 'create'})),
     path('login', LoginView.as_view()),
