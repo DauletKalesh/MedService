7a4e33c304ec2ee7f3559a710ff813fa245b3e90
Зарина <Zarina@MacBook-Pro-Zarina.local>
2022-12-20 14:39:12 +0600

    z

--- med_management/pdf_service.py
+++ med_management/pdf_service.py
@@ -1,30 +1,28 @@
-import jinja2
-import pdfkit
+import io
+from django.http import FileResponse
+from django.template.loader import get_template
+import trml2pdf
+
 from datetime import datetime
+
 def create_pdf(user_data):
-    medical_history = user_data.med_history.first()
+    medical_history = user_data.med_history.all()
+    for i in medical_history:
+       print(i.symptoms)
     first_name = user_data.user.first_name
     last_name = user_data.user.last_name
     email = user_data.user.email
-    date = today_date = datetime.today().strftime("%d %b, %Y")
+    date = datetime.today().strftime("%d %b, %Y")
     date_of_record = 'frw'
     symptoms = 'dfdgdd'
     diagnosis = 'dslkdfjkldfjkl'
 
-    context = {'first_name': first_name, 'last_name': last_name, 'email': email, 'date': date,
-           'date_of_record': date_of_record, 'symptoms': symptoms, 'diagnosis':diagnosis}
-    print(context)
 
-    template_loader = jinja2.FileSystemLoader('./')
-    print(template_loader)
-    template_env = jinja2.Environment(loader=template_loader)
-    print(template_env)
-
-    html_template = 'basic-template.html'
-    template = template_env.get_template(html_template)
-    print(template)
-    output_text = template.render(context)
-
-    config = pdfkit.configuration(wkhtmltopdf='/usr/local/bin/wkhtmltopdf')
-    output_pdf = 'pdf_generated.pdf'
-    pdfkit.from_string(output_text, output_pdf, configuration=config, css='style.css')
+    data = {'first_name': first_name, 'last_name': last_name, 'email': email, 'date': date,
+           'date_of_record': date_of_record, 'symptoms': symptoms, 'diagnosis':diagnosis, 'medical_history': medical_history,}
+    
+    template = get_template('./pdf_structure.rml')
+    xmlstring = template.render(data)
+    pdfstr = trml2pdf.parseString(xmlstring)
+    return pdfstr
+#     return FileResponse(pdfstr, as_attachment=True, filename='{}_{}_medical_history_{}.pdf'.format(first_name, last_name, datetime.today().strftime("%d %b, %Y")))
